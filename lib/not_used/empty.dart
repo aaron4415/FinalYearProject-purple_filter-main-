2312eef7b5b76b56f771d5aef0ffe3b6f7f9a359
alexyuyl <71703442+alexyuyl@users.noreply.github.com>
2023-04-11 16:47:56 +0800

    Create empty.dart

---
+++ lib/not_used/empty.dart
@@ -0,0 +1,16 @@
+// child: Padding(
+// padding: EdgeInsets.only(right: width / 120),
+// child: BorderedText(
+// strokeWidth: 4.0,
+// strokeColor: Colors.blue,
+// child: Text(
+// LocaleKeys.changeInterface.tr(),
+// style: TextStyle(
+// color: Colors.white,
+// fontSize: height / 30,
+// fontStyle: FontStyle.italic,
+// fontWeight: FontWeight.bold,
+// ),
+// textAlign: TextAlign.left,
+// ),
+// )),
